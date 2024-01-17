c12c16e427c9cf696a653e77d955bd9f2c847de1
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-18 03:49:31 +0600

    banner bg img host changed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,14 +6,15 @@ const Banner = () => {
       className="hero min-h-screen"
       style={{
         backgroundImage:
-          'url(https://img.freepik.com/free-photo/standard-quality-control-concept-m_23-2150041864.jpg?w=1800&t=st=1705486741~exp=1705487341~hmac=c32467229e543eef892f559bb04bd8b468a9204f79e341ed5c6a51dd8549a2d7)',
+          'url(https://i.postimg.cc/bNXFcBwf/rm373batch13-085.jpg)',
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-2 text-4xl font-bold py-3 md:py-10 uppercase leading-tight">
-          Weave Your Future with <span className='text-blue-400'>SkillCraze!</span>
+            Weave Your Future with{' '}
+            <span className="text-blue-400">SkillCraze!</span>
           </h1>
 
           <Link to="/dashboard">
