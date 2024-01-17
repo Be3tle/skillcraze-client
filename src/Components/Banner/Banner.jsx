2cdd5e10dd40bb98d63de59cdcf2993579217182
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-17 16:21:12 +0600

    changed banner bg

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
       className="hero min-h-screen"
       style={{
         backgroundImage:
-          'url(https://i.postimg.cc/RF6JtLz8/system-developers-analyzing-code-wall-screen-tv-looking-errors-while-team-coders-collaborate-artific.jpg)',
+          'url(https://img.freepik.com/free-photo/standard-quality-control-concept-m_23-2150041864.jpg?w=1800&t=st=1705486741~exp=1705487341~hmac=c32467229e543eef892f559bb04bd8b468a9204f79e341ed5c6a51dd8549a2d7)',
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
