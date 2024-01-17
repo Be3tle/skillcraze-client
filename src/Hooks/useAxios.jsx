69af242b80cc4bbd37c7f146b3da7d00ec214ddf
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-18 03:50:56 +0600

    axios variable updated

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const axios = axios.create({
+const axiosPublic = axios.create({
   baseURL: 'http://localhost:3000',
 });
 
 const useAxios = () => {
-  return axios;
+  return axiosPublic;
 };
 
 export default useAxios;
