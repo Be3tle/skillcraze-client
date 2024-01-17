bbda97cb5754e68bae3dcaa75ffab51ea30b7b52
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-17 14:51:26 +0600

    changed navbar style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 
 const Navbar = () => {
@@ -7,78 +8,86 @@ const Navbar = () => {
   const handleLogOut = () => {
     logOut()
       .then(() => {})
-      .catch((error) => console.log(error));
+      .catch((error) => console.error(error));
   };
 
+  const navItems = (
+    <div className="md:flex text-base">
+      <li>
+        <a href="/">Home</a>
+      </li>
+
+      <li>
+        <a
+          href="dashboard
+        "
+        >
+          Dashboard
+        </a>
+      </li>
+
+      <li>
+        <a href="">Blog</a>
+      </li>
+
+      <li>
+        <a href="">About Us</a>
+      </li>
+    </div>
+  );
+
   return (
-    <div className="">
-      <div className="navbar md:fixed z-10 bg-opacity-70 md:bg-opacity-30  bg-black text-white max-w-screen-2xl mx-auto">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+    <div className="navbar md:fixed z-10 bg-opacity-70 md:bg-opacity-30  bg-black text-white max-w-screen-2xl mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <a href="">All Courses</a>
-              </li>
-
-              <li>
-                <a href="/dashboard">Dashboard</a>
-              </li>
-
-              <li>
-                <a href="login">Login</a>
-              </li>
-            </ul>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <a href="/" className="btn btn-ghost text-xl ml-32 md:ml-0">
-            <img src="../../../public/assets/logo.jpeg" alt="" />
-          </a>
-        </div>
-        {/* <div className="navbar-end hidden lg:flex"></div> */}
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a href="">All Courses</a>
-            </li>
-
-            <li>
-              <a href="/dashboard">Dashboard</a>
-            </li>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-neutral rounded-box w-52"
+          >
+            {navItems}
           </ul>
-          {user ? (
-            <a
-              onClick={handleLogOut}
-              className="border-red-600 bg-transparent border-2 hover:bg-transparent hover:border-red-700 px-3 py-1 text-white font-normal rounded-md"
-            >
-              Logout
-            </a>
-          ) : (
-            <a
-              href="login"
-              className="border-red-600 bg-transparent border-2 hover:bg-transparent hover:border-red-700 px-3 py-1 text-white font-normal rounded-md"
-            >
-              Login
-            </a>
-          )}
         </div>
+        <Link to="/">
+          <div>
+            <img className="w-12" src="/assets/logo.jpeg" alt="" />
+          </div>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        {user ? (
+          <a
+            onClick={handleLogOut}
+            className="btn bg-gray-500 text-white hover:bg-gray-700 border-0"
+          >
+            Log Out
+          </a>
+        ) : (
+          <a
+            href="login"
+            className="btn bg-gray-500 text-white hover:bg-gray-700 border-0"
+          >
+            Log In
+          </a>
+        )}
       </div>
     </div>
   );
