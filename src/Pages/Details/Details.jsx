cd20564782b9bd5613b151c63b04a4bd5c2f09fc
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-18 11:52:18 +0600

    single course get api

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxios from '../../Hooks/useAxios';
+import DetailsCard from './DetailsCard';
+import { useParams } from 'react-router-dom';
 
 const Details = () => {
-  return <div>Details</div>;
+  const { id } = useParams();
+  const axios = useAxios();
+  const [course, setCourse] = useState([]);
+
+  // console.log(book);
+  useEffect(() => {
+    axios.get(`/courses/${id}`).then((res) => {
+      setCourse(res.data);
+    });
+  }, [id]);
+  return (
+    <div>
+      <DetailsCard course={course}></DetailsCard>
+    </div>
+  );
 };
 
 export default Details;
